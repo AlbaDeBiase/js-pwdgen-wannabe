0582c21d64ca00059711ee71282e3fb7e710106a
Alba De biase <debiasealba@gmail.com>
2020-10-12 17:36:29 +0200

    variabili

--- progetto.js
+++ progetto.js
@@ -1,5 +1,11 @@
 var nomeUtente = prompt ('Qual è il tuo nome?');
+console.console.log(test);
+document.getElementById('nome').innerHTML;
 
 var cognomeUtente = prompt('Qual è il tuo cognome?');
+console.console.log(testdue);
+document.getElementById('cognome').innerHTML;
 
 var colorePreferito = prompt('Qual è il tuo colore preferito?');
+console.console.log(testtre);
+document.getElementById('colore').innerHTML;
